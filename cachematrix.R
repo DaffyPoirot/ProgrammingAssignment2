021104d28cb2e67f01807ac97a1950465c488cdb
DaffyPoirot <65905421+DaffyPoirot@users.noreply.github.com>
2020-06-03 22:04:38 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# Plot the 30-day mortality rates for heart attack
 
+# Reading in data
+outcome <- data.table::fread('outcome-of-care-measures.csv')
+outcome[, (11) := lapply(.SD, as.numeric), .SDcols = (11)]
+outcome[, lapply(.SD
+                 , hist
+                 , xlab= "Deaths"
+                 , main = "Hospital 30-Day Death (Mortality) Rates from Heart Attack"
+                 , col="lightblue")
+        , .SDcols = (11)]
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
